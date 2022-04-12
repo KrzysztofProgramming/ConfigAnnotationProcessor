aeb8ab9059eac214ebacf8c83e174c240546da0a
KrzysztofProgramming <krzychuk112@gmail.com>
2022-04-12 18:08:56 +0200

    updated dependencies

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,6 +6,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(ConfigProcessor.splitOnNChar(3, '_', "siema_elo_ko_z_ak")));
+
     }
 }
